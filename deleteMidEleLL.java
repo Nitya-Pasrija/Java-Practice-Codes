a717079ddabcaf98af45da6b0bc14b17e076c814
Nitya Pasrija <97171261+Nitya-Pasrija@users.noreply.github.com>
2023-01-17 23:22:40 +0530

    java codes

---
+++ deleteMidEleLL.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null)
+            return null;
+            
+        int count = 0;
+        ListNode p1 = head, p2 = head;
+        while (p1 != null) {
+            count += 1;
+            p1 = p1.next;
+        }
+        int middleIndex = count / 2;
+        for (int i = 0; i < middleIndex - 1; ++i)
+            p2 = p2.next;
+        p2.next = p2.next.next;
+        return head;
+    }
+}
